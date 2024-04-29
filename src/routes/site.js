d0aeb69640bbd33d16fe230b313a71d17cdafbec
Nguyen Huu Hoang Nam <nhhnam0405@gmail.com>
2024-04-29 10:19:37 +0700

    Avatar UI

--- src/routes/site.js
+++ src/routes/site.js
@@ -5,7 +5,6 @@ const siteController = require('../app/controllers/SiteController')
 router.post('/store', siteController.store)
 router.post('/login', siteController.login)
 router.get('/logout', siteController.logout)
-
 router.get('/', siteController.home)
 
 module.exports = router
