eb36c1c329de7df7208386e4e6a79de6b92585a5
Nam Nguyen <nhhnam0405@gmail.com>
2024-05-28 23:47:19 +0700

    render product UI

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -1,8 +1,12 @@
-
+const Product = require('../models/Product')
 class ProducController {
     // [GET] /products/:slug
     product_info(req,res, next) {
-       res.render('product')
+        Product.findOne({slug: req.params.slug})
+            .then(product => {
+                res.render("product", {product})
+            })
+            .catch(next);
     }
 
 }
