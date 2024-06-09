58e539c84ea17cd88778a849ed5309b86d7b7bbf
Nguyen Huu Hoang Nam <nhhnam0405@gmail.com>
2024-06-09 11:56:00 +0700

    render seller of product

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -1,11 +1,20 @@
 const Product = require('../models/Product')
+const User = require('../models/User')
 class ProducController {
     // [GET] /products/:slug
     product_info(req,res, next) {
         Product.findOne({slug: req.params.slug})
             .then(product => {
-                console.log(product)
-                res.render("product", {product})
+                User.findOne({_id: product.seller_id})
+                    .then(user => {
+                        const data = {
+                            account: req.session.account || null,
+                            avatar: req.session.avatar
+                        };
+                    
+                        res.render("product", {product, data, user})
+                    })
+                    .catch(next);  
             })
             .catch(next);
     }
